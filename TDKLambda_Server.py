2a2a57ee196ffb193814d7bf8238de54cf576611
Andrey Sanin <sanin_andrey@mail.ru>
2019-11-30 00:29:56 +0700

    commit

--- TDKLambda_Server.py
+++ TDKLambda_Server.py
@@ -21,10 +21,11 @@ else:
 
 import tango
 from tango import AttrQuality, AttrWriteType, DispLevel, DevState, DebugIt
-from tango.server import Device, attribute, command, pipe, device_property
+from tango.server import Device, attribute, command, device_property
 
 
 class TDKLamda(Device):
+    ports = []
     devices = []
 
     devicetype = attribute(label="type", dtype=str,
@@ -37,7 +38,7 @@ class TDKLamda(Device):
                         display_level=DispLevel.OPERATOR,
                         access=AttrWriteType.READ_WRITE,
                         unit="", format="",
-                        doc="device type")
+                        doc="on/off")
 
     voltage = attribute(label="Voltage", dtype=float,
                         display_level=DispLevel.OPERATOR,
@@ -53,7 +54,7 @@ class TDKLamda(Device):
 
     current = attribute(label="Current", dtype=float,
                         display_level=DispLevel.OPERATOR,
-                        access=AttrWriteType.READ_WRITE,
+                        access=AttrWriteType.READ,
                         unit="A", format="8.4f",
                         doc="Measured current")
 
@@ -63,11 +64,76 @@ class TDKLamda(Device):
                         unit="A", format="8.4f",
                         doc="Programmed current")
 
-    host = device_property(dtype=str)
-    port = device_property(dtype=int, default_value=9788)
+    def get_device_property(self, prop: str, default=None):
+        name = self.get_name()
+        # device proxy
+        dp = tango.DeviceProxy(name)
+        # read property
+        pr = dp.get_property(prop)[prop]
+        result = None
+        if len(pr) > 0:
+            result = pr[0]
+        if default is None:
+            return result
+        try:
+            if result is None or result == '':
+                result = default
+            else:
+                result = type(default)(result)
+        except:
+            return result
 
     def init_device(self):
+        if hasattr(self, 'port') and self.port is not None:
+            self.port.close()
+            self.port = None
+            self.addr = None
+        self.set_state(DevState.INIT)
         Device.init_device(self)
+        # get port and arrdess from property
+        self.port = self.get_device_property('port')
+        self.addr = self.get_device_property('addr')
+        # if port or address is not defined
+        if self.port is None or self.addr is None:
+            msg = 'Address or port not defined for %s' % self
+            print(msg)
+            self.error_stream(msg)
+            self.set_state(DevState.FAULT)
+            return
+        # check if port an addr are in use
+        for d in TDKLamda.devices:
+            if d.port == self.port and d.addr == self.addr:
+                msg = 'Address %s is in use for port %s device %s' % (self.addr, self.port, self)
+                print(msg)
+                self.error_stream(msg)
+                self.set_state(DevState.FAULT)
+                return
+        if len(TDKLamda.ports) == 0:
+            TDKLamda.ports = comports()
+        found = False
+        for p in TDKLamda.ports:
+            if p.name == self.port:
+                found = True
+        if not found:
+            msg = 'COM port %s does not exist for %s' % (self.port, self)
+            print(msg)
+            self.error_stream(msg)
+            self.set_state(DevState.FAULT)
+            return
+        # create TDKLamda device
+        self.com = serial.Serial(self.port, baudrate=9600, timeout=0)
+        # create variables
+        self.error_count = 0
+        self.time = None
+        self.reconnect_timeout = int(self.get_device_property('reconnect_timeout', 5000))
+        # add device to list
+        TDKLamda.devices.append(self)
+        msg = 'TDKLamda device at %s %d has been created' % (self.port, self.addr)
+        print(msg)
+        self.info_stream(msg)
+
+
+
         db = tango.Database()
         try:
             self.device_info = db.get_device_info('test/power_supply/1')
@@ -77,6 +143,16 @@ class TDKLamda(Device):
         self.__current = 0.0
         self.set_state(DevState.STANDBY)
 
+
+        # if device type is recognized
+        if self.et._name != 0:
+            # set state to running
+            self.set_state(DevState.RUNNING)
+        else:
+            # unknown device type
+            self.set_state(DevState.FAULT)
+
+
     def read_voltage(self):
         self.info_stream("read_voltage(%s, %d)", self.host, self.port)
         return 9.99, time.time(), AttrQuality.ATTR_WARNING
@@ -84,19 +160,19 @@ class TDKLamda(Device):
     def read_current(self):
         return self.__current
 
-    def read_set_voltage(self):
+    def read_prorgammed__voltage(self):
         # should set the power supply current
         self.__current = 0
 
-    def read_set_current(self):
+    def read_prorgammed__current(self):
         # should set the power supply current
         self.__current = 0
 
-    def write_set_voltage(self, v):
+    def write_prorgammed__voltage(self, v):
         # should set the power supply current
         self.__current = 0
 
-    def write_set_current(self, c):
+    def write_prorgammed__current(self, c):
         # should set the power supply current
         self.__current = 0
 
@@ -128,4 +204,4 @@ class TDKLamda(Device):
 
 
 if __name__ == "__main__":
-    PowerSupply.run_server()
+    TDKLamda.run_server()
