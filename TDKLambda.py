c86dc0bb3bd0c372e0d970e6035be22fd6e53990
Andrey Sanin <sanin_andrey@mail.ru>
2019-12-05 08:14:02 +0700

    commit

--- TDKLambda.py
+++ TDKLambda.py
@@ -13,7 +13,7 @@ elif os.name == 'posix':
 else:
     raise ImportError("No implementation for platform ('{}') is available".format(os.name))
 
-MAX_TIMEOUT = 3.0   # sec
+MAX_TIMEOUT = 1.5   # sec
 MIN_TIMEOUT = 0.1   # sec
 RETRIES = 3
 SUSPEND = 5.0
@@ -22,8 +22,9 @@ class TDKLambda():
     devices = []
     ports = []
 
-    def __init__(self, port: str, addr=6, checksum=False, logger=None):
+    def __init__(self, port: str, addr=6, checksum=False, baudrate=9600, timeout=0, logger=None):
         # create variables
+        self.last_command = b''
         self.com = None
         self.checksum = checksum
         self.time = time.time()
@@ -43,14 +44,15 @@ class TDKLambda():
                 msg = 'Address %s is in use for port %s device %s' % (self.addr, self.port, self)
                 self.logger.error(msg)
                 return
+        # assign com port
         for p in TDKLambda.ports:
             # com port exists
             if p.name == self.port:
                 self.com = p
         if self.com is None:
-            # create port
+            # create new port
             try:
-                self.com = serial.Serial(self.port, baudrate=9600, timeout=0)
+                self.com = serial.Serial(self.port, baudrate=baudrate, timeout=timeout)
                 TDKLambda.ports.append(self.port)
             except:
                 self.com = None
@@ -64,6 +66,14 @@ class TDKLambda():
         # initialize device type
         #self.type = self.read_devicetype()
 
+    @staticmethod
+    def checksum(cmd):
+        s = 0
+        for b in cmd:
+            s += int(b)
+        result = str.encode(hex(s)[-2:])
+        return result.upper()
+
     def send_command(self, cmd):
         if self.com is None or time.time() < self.suspend:
             return b''
@@ -71,11 +81,11 @@ class TDKLambda():
             cmd = str.encode(cmd)
         if cmd[-1] != b'\r':
             cmd += b'\r'
+        self.last_command = cmd
         self.com.reset_input_buffer()
         self.com.write(cmd)
         result = self.read_to_cr()
-        if result is not None:
-            return result
+        return result
 
     def _read(self):
         if self.com is None or time.time() < self.suspend:
@@ -87,8 +97,10 @@ class TDKLambda():
             if dt > self.timeout:
                 self.timeout = min(1.5*dt, MAX_TIMEOUT)
                 self.timeout_flag = True
+                # resend command
+                self.send_command(self.last_command)
                 return None
-            data = self.com.read()
+            data = self.com.read(100)
             dt = time.time() - time0
         self.time = time.time()
         self.suspend = time.time()
@@ -105,7 +117,7 @@ class TDKLambda():
             data = self._read()
             count -= 1
             if count < 0:
-                msg = 'No response from %s %d - suspended' % (self.port, self.addr)
+                msg = 'No response from %s addr %d - suspended' % (self.port, self.addr)
                 self.logger.error(msg)
                 self.suspend = time.time() + SUSPEND
                 return None
