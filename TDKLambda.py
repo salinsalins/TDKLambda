0f167a9bdcc3dd096a459b902a7e5945491be3ef
Sanin <sanin_andrey@mail.ru>
2019-12-06 11:47:55 +0700

    commit

--- TDKLambda.py
+++ TDKLambda.py
@@ -25,6 +25,8 @@ class TDKLambda():
 
     def __init__(self, port: str, addr=6, checksum=False, baudrate=9600, timeout=0, logger=None):
         # create variables
+        self.last_response = b''
+        self.unexpected_count = 0
         self.auto_addr = True
         self.last_command = b''
         self.com = None
@@ -65,8 +67,8 @@ class TDKLambda():
         # set device address and check 'OK' response
         self.online = self.set_addr()
         # initialize device type and serial number
-        self.id = self.send_command(b'IDN?')
-        self.serial_number = self.send_command(b'SN?')
+        self.id = self._send_command(b'IDN?')
+        self.serial_number = self._send_command(b'SN?')
         # add device to list
         TDKLambda.devices.append(self)
         msg = 'TDKLambda device at %s %d has been created' % (self.port, self.addr)
@@ -80,7 +82,7 @@ class TDKLambda():
         result = str.encode(hex(s)[-2:])
         return result.upper()
 
-    def send_command(self, cmd):
+    def _send_command(self, cmd):
         if self.com is None or time.time() < self.suspend:
             return b''
         if isinstance(cmd, str):
@@ -107,6 +109,11 @@ class TDKLambda():
             result = self.read_to_cr()
         return result
 
+    def send_command(self, cmd):
+        if self.auto_addr:
+            self.set_addr()
+        return self._send_command(cmd)
+
     def _read(self):
         if self.com is None or time.time() < self.suspend:
             return None
@@ -121,7 +128,7 @@ class TDKLambda():
                 msg = 'Timeout increase to %f' % self.timeout
                 self.logger.info(msg)
                 # resend command
-                self.send_command(self.last_command)
+                self._send_command(self.last_command)
                 return None
             data = self.com.read(10000)
             dt = time.time() - time0
@@ -161,49 +168,74 @@ class TDKLambda():
         while data is not None:
             result += data
             if b'\r' in data:
+                n = result.find(b'\r')
+                self.last_response = result[:n]
                 #dt = time.time() - time0
-                #print('read_to_cr', dt)
+                #print('read_to_cr1', dt)
                 return result
             data = self.read()
+        self.last_response = result
         #dt = time.time() - time0
-        #print('read_to_cr', dt)
+        #print('read_to_cr2', dt)
         return result
 
     def set_addr(self):
-        result = self.send_command(b'ADR %d\r' % self.addr)
+        result = self._send_command(b'ADR %d\r' % self.addr)
         if not result.startswith(b'OK'):
-            self.unexpected_reply(result)
+            self.check_response(result)
             return False
         return True
 
-    def read_value(self, cmd=b'MV?'):
+    def read_float(self, cmd=b'MV?'):
         #t0 = time.time()
-        if self.auto_addr:
-            self.set_addr()
         reply = b''
         try:
             reply = self.send_command(cmd)
             v = float(reply)
         except:
-            self.unexpected_reply(reply)
+            self.check_response(reply)
             v = float('Nan')
         #dt = time.time() - t0
         #print('read_value', dt)
         return v
 
-    def write_value(self, cmd=b'PV', value=0.0):
-        if self.auto_addr:
-            self.set_addr()
-        cmd = cmd.upper() + b' ' + str.encode(str(value))[:10] + b'\r'
-        result = self.send_command(cmd)
-        if not result.startswith(b'OK\r'):
-            self.unexpected_reply(result)
-            return False
-        return True
+    def read_value(self, cmd=b'MV?', vtype=float):
+        #t0 = time.time()
+        reply = b''
+        try:
+            reply = self.send_command(cmd)
+            v = vtype(reply)
+        except:
+            self.check_response(b'!?!')
+            v = None
+        #dt = time.time() - t0
+        #print('read_value', dt)
+        return v
 
-    def unexpected_reply(self, reply=b''):
-        msg = 'Unexpected reply %s from %s : %d' % (reply, self.port, self.addr)
-        self.logger.error(msg)
+    def read_bool(self, cmd=b'OUT?'):
+        response = self.send_command(cmd)
+        if response.upper() in (b'ON', b'1'):
+            return True
+        if response.upper() in (b'OFF', b'0'):
+            return False
+        msg = 'Umbigous response %s for boolean, False returned.' % response
+        self.logger.warning(msg)
+        return False
+
+    def write_value(self, cmd=b'PV', value='', response=b'OK\r'):
+        cmd = cmd.upper() + b' ' + str.encode(str(value))[:10] + b'\r'
+        self.send_command(cmd)
+        return self.check_response(response)
+
+    def check_response(self, expect=b'OK\r', response=None):
+        if response is None:
+            response = self.last_response
+        if not response.startswith(expect):
+            self.unexpected_count += 1
+            msg = 'Unexpected reply %s from %s : %d' % (response, self.port, self.addr)
+            self.logger.error(msg)
+            return False
+        self.unexpected_count = 0
         return True
 
 if __name__ == "__main__":
@@ -211,6 +243,6 @@ if __name__ == "__main__":
     pdl = TDKLambda("COM3", 6)
     while True:
         t0 = time.time()
-        v = pdl.read_value("PC?")
+        v = pdl.read_float("PC?")
         dt = time.time()-t0
         print(dt, v, pdl.timeout)
