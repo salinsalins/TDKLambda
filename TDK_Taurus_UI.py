90d83f095332a4bb79ee10207687215cf480e002
Sanin <sanin_andrey@mail.ru>
2019-12-09 13:46:12 +0700

    commit

--- TDK_Taurus_UI.py
+++ TDK_Taurus_UI.py
@@ -1,26 +1,13 @@
 import sys
-from taurus.external.qt import Qt
+from taurus.qt.qtgui.panel import TaurusForm
 from taurus.qt.qtgui.application import TaurusApplication
 
-from taurus.qt.qtgui.display import TaurusLabel, TaurusLed
-from taurus.qt.qtgui.input import TaurusValueCheckBox, TaurusValueSpinBoxEx
+app = TaurusApplication(sys.argv, cmd_line_parser=None)
 
-app = TaurusApplication(sys.argv, cmd_line_parser=None,)
-panel = Qt.QWidget()
-layout = Qt.QVBoxLayout()
-panel.setLayout(layout)
+attrs = ['voltage', 'current', 'out', 'programmed_voltage', 'programmed_current']
+model = ['binp/nbi/magnet1/%s' % attr for attr in attrs]
 
-layout1 = Qt.QHBoxLayout()
-led = TaurusLed()
-layout1.addWidget(led)
-led.model = 'binp/nbi/magnet1/out'
-v = TaurusLabel()
-layout1.addWidget(v)
-v.model = 'binp/nbi/magnet1/voltage'
-c = TaurusLabel()
-layout1.addWidget(c)
-c.model = 'binp/nbi/magnet1/current'
-
-layout.addChildLayout(layout)
-panel.show()
+w = TaurusForm()
+w.model = model
+w.show()
 sys.exit(app.exec_())
