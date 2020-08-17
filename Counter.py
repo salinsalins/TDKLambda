ede00f6385c6cf8cb54b078a8451fc1ffdafead4
salinsalins <sanin_andrey@mail.ru>
2020-08-17 14:25:34 +0700

    commit

---
+++ Counter.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+class Counter:
+    def __init__(self, limit=0, action=None, *args, **kwargs):
+        self.value = 0
+        self.limit = limit
+        self.action = action
+        self.args = args
+        self.kwargs = kwargs
+
+    def clear(self):
+        self.value = 0
+
+    def inc(self):
+        self.value += 1
+        self.act()
+
+    def check(self):
+        return self.value > self.limit
+
+    def act(self, action=None):
+        if action is None:
+            action = self.action
+        if self.check():
+            self.value = 0
+            if action is not None:
+                return action(*self.args, **self.kwargs)
+            else:
+                return True
+        else:
+            return False
+
+    def __iadd__(self, other):
+        self.value += other
+        self.act()
+        return self
--- TDKLambda.py
+++ TDKLambda.py
@@ -36,49 +36,14 @@ class MoxaTCPComPort:
 
 addressInUseException = Exception('Address is in use')
 wrongAddressException = Exception('Wrong address')
+portCreationError = Exception('COM port creation error')
 
 CR = b'\r'
 
 
-class Counter:
-    def __init__(self, limit=0, action=None, *args, **kwargs):
-        self.value = 0
-        self.limit = limit
-        self.action = action
-        self.args = args
-        self.kwargs = kwargs
-
-    def clear(self):
-        self.value = 0
-
-    def inc(self):
-        self.value += 1
-        self.act()
-
-    def check(self):
-        return self.value > self.limit
-
-    def act(self, action=None):
-        if action is None:
-            action = self.action
-        if self.check():
-            self.value = 0
-            if action is not None:
-                return action(*self.args, **self.kwargs)
-            else:
-                return True
-        else:
-            return False
-
-    def __iadd__(self, other):
-        self.value += other
-        self.act()
-        return self
-
-
 class TDKLambda:
     LOG_LEVEL = logging.DEBUG
-    EMULATE = False
+    EMULATE = True
     max_timeout = 0.8  # sec
     min_timeout = 0.15  # sec
     RETRIES = 3
