769eb910ca813200eaecc60617dad008bb36029d
salinsalins <sanin_andrey@mail.ru>
2020-08-28 12:45:54 +0700

    commit

--- Async/AsyncTest.py
+++ Async/AsyncTest.py
@@ -4,21 +4,23 @@ import time
 import logging
 import asyncio
 
-async def looper():
+
+async def looper(delay=0.5):
     loop = asyncio.get_running_loop()
     while True:
         tasks = asyncio.all_tasks()
         for task in tasks:
             print(task)
-            #print(task.get_name())
+            # print(task.get_name())
         print(time.time() - t0, loop.is_running(), len(tasks))
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(delay)
+
 
 async def main():
     task1 = asyncio.create_task(looper())
     while True:
         await asyncio.sleep(0)
-    #await asyncio.wait({task1})
+    # await asyncio.wait({task1})
 
 if __name__ == "__main__":
     logging.getLogger("asyncio").setLevel(logging.DEBUG)
