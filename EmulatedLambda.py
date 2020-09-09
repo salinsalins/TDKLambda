3c5f527bad721779d8d7e479d725c3d6e47300db
Andrey Sanin <sanin_andrey@mail.ru>
2020-09-10 01:19:05 +0700

    commit

--- EmulatedLambda.py
+++ EmulatedLambda.py
@@ -24,6 +24,7 @@ class FakeComPort:
         FakeComPort.SN += 1
         self.id = {self.last_address: FakeComPort.ID}
         self.t = {self.last_address: time.perf_counter()}
+        self.write_error = False
 
     def close(self):
         self.last_write = b''
@@ -32,6 +33,7 @@ class FakeComPort:
 
     def write(self, cmd, timeout=None):
         self.last_write = cmd
+        self.write_error = False
         try:
             if self.last_write.startswith(b'ADR '):
                 self.last_address = int(self.last_write[4:])
@@ -52,11 +54,14 @@ class FakeComPort:
                 self.out[self.last_address] = True
             elif self.last_write.startswith(b'OUT OF') or self.last_write.startswith(b'OUT 0'):
                 self.out[self.last_address] = False
+            else:
+                self.write_error = True
             self.t[self.last_address] = time.perf_counter()
             return len(cmd)
         except:
+            self.write_error = True
             self.t[self.last_address] = time.perf_counter()
-            return 0
+            return len(cmd)
 
     def read(self, size=1, timeout=None):
         if self.last_write == b'':
@@ -64,8 +69,11 @@ class FakeComPort:
         if time.perf_counter() - self.t[self.last_address] < self.RESPONSE_DELAY:
             return b''
         self.t[self.last_address] = time.perf_counter()
+        if self.write_error:
+            self.last_write = b''
+            return b'E1\r'
         if self.last_write.startswith(b'ADR '):
-            self.last_address = int(self.last_write[3:])
+            # self.last_address = int(self.last_write[3:])
             self.last_write = b''
             return b'OK\r'
         if self.last_write.startswith(b'DVC?'):
