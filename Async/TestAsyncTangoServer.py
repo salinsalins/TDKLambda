8db0145d72030309573dacd757dea053bd8f3237
salinsalins <sanin_andrey@mail.ru>
2020-12-10 16:55:56 +0700

    commit

--- Async/TestAsyncTangoServer.py
+++ Async/TestAsyncTangoServer.py
@@ -2,6 +2,8 @@
 
 import logging
 import asyncio
+import time
+
 from tango import DevState, GreenMode
 from tango.server import Device, command, attribute
 
@@ -12,7 +14,7 @@ class AsyncioDevice(Device):
     async def init_device(self):
         await super().init_device()
         self.value = 0.0
-        self.set_state(DevState.ON)
+        self.set_state(DevState.RUNNING)
 
     @command
     async def long_running_command(self):
@@ -32,20 +34,26 @@ class AsyncioDevice(Device):
 
     @attribute
     async def test_attribute(self):
-        logger.info('Entry')
-        await asyncio.sleep(2)
-        logger.info('Exit')
+        logger.info('Entry %s', self)
+        await asyncio.sleep(0.5)
+        logger.info('Exit %s', self)
         return self.value
 
     @test_attribute.write
     async def write_test_attribute(self, value):
-        logger.info('Entry')
+        global n
+        n1 = n
+        n += 1
+        logger.info('Entry %s', n1)
         self.value = value
-        await asyncio.sleep(0.5)
-        logger.info('Exit')
+        time.sleep(1.5)
+        #await asyncio.sleep(0.5)
+        logger.info('Exit %s', n1)
 
+n = 0
 
 if __name__ == '__main__':
+    # configure logger
     logger = logging.getLogger(__name__)
     logger.propagate = False
     logger.setLevel(logging.DEBUG)
@@ -56,5 +64,6 @@ if __name__ == '__main__':
     console_handler.setFormatter(log_formatter)
     logger.addHandler(console_handler)
 
+    # run server
     AsyncioDevice.run_server()
 
