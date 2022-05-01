6722955921bd6dc73cc82727b28513390dd48986
Andrey Sanin <sanin_andrey@mail.ru>
2022-05-02 00:25:04 +0700

    commit

--- Terminal.py
+++ Terminal.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-
+from datetime import datetime
 import os.path
 import sys
 import time
@@ -36,6 +36,7 @@ class MainWindow(QMainWindow):
         # members definition
         self.port = None
         self.baud = 9600
+        self.connected = False
         # Load the Qt UI
         uic.loadUi(UI_FILE, self)
         # Default main window parameters
@@ -53,18 +54,34 @@ class MainWindow(QMainWindow):
             h += hex(i)[2:] + ' '
         self.lineEdit_4.setText(h)
         #
-        self.port = str(self.lineEdit.text())
-        self.baud = int(self.lineEdit_2.text())
-        self.com = serial.Serial(self.port, baudrate=self.baud, timeout=0)
+        # self.port = str(self.lineEdit.text())
+        # self.baud = int(self.lineEdit_2.text())
+        # self.com = serial.Serial(self.port, baudrate=self.baud, timeout=0)
+        self.connect_port()
         # Connect signals with slots
         self.lineEdit_3.editingFinished.connect(self.send_changed)
         self.lineEdit_4.editingFinished.connect(self.hex_changed)
         self.pushButton.clicked.connect(self.button_clicked)
+        self.pushButton_2.clicked.connect(self.connect_port)
         # Defile callback task and start timer
         self.timer = QTimer()
         self.timer.timeout.connect(self.timer_handler)
         self.timer.start(TIMER_PERIOD)
         self.logger.info('\n---------- Config Finished -----------\n')
+
+    def connect_port(self):
+        try:
+            self.com.close()
+        except:
+            pass
+        try:
+            self.port = str(self.lineEdit.text())
+            self.baud = int(self.lineEdit_2.text())
+            self.com = serial.Serial(self.port, baudrate=self.baud, timeout=0)
+            self.connected = True
+            self.plainTextEdit_2.appendPlainText('%s Port %s connected successfully' % (dts(), self.port))
+        except:
+            self.plainTextEdit_2.setPlainText('Port %s connection error' % self.port)
 
     def hex_from_str(self, v):
         h = ''
@@ -92,11 +109,20 @@ class MainWindow(QMainWindow):
         # self.logger.debug('%s of %s bytes written', stat, len(ve))
 
     def button_clicked(self):
+        if not self.connected:
+            return
         v = self.lineEdit_3.text().encode()
         s = self.str_from_hex(self.lineEdit_5.text()).encode()
         t = v + s
         stat = self.com.write(t)
-        self.logger.debug('%s of %s bytes written %s', stat, len(t), t)
+        dt = dts()
+        if stat == len(t):
+            msg = dt + ' %s bytes written: %s' % (stat, t)
+            self.logger.debug(msg)
+        else:
+            msg = dt + ' %s of %s bytes written from %s' % (stat, len(t), t)
+            self.logger.debug(msg)
+        self.plainTextEdit_2.appendPlainText(msg)
 
     def hex_changed(self):
         v = self.lineEdit_4.text()
@@ -116,18 +142,29 @@ class MainWindow(QMainWindow):
         save_settings(self, file_name=CONFIG_FILE, widgets=(self.lineEdit, self.lineEdit_2, self.lineEdit_3, self.lineEdit_5))
 
     def timer_handler(self):
+        if not self.connected:
+            self.plainTextEdit_2.setPlainText('Port %s connection error' % self.port)
+            return
         result = b''
         r = self.com.read(1)
         while len(r) > 0:
             result += r
             r = self.com.read(1)
         if len(result) > 0:
-            self.logger.debug('received %s bytes : %s', len(result), str(result))
-            self.plainTextEdit.setPlainText(str(result))
+            self.logger.debug('received %s bytes: %s', len(result), str(result))
+            dt = dts()
+            self.plainTextEdit_2.appendPlainText('%s received %s bytes: %s' % (dt, len(result), result))
+            self.plainTextEdit.appendPlainText('\n%s received %s bytes:' % (dt, len(result)))
+            self.plainTextEdit.appendPlainText(str(result))
             h = ''
             for i in result:
                 h += hex(i) + ' '
-            self.plainTextEdit_2.setPlainText(h)
+            self.plainTextEdit.appendPlainText(h)
+
+
+def dts():
+    #return datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    return datetime.utcnow().strftime('%H:%M:%S.%f')[:-3]
 
 
 if __name__ == '__main__':
