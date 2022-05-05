52729091ed76ed38958b807e692f85a7ef4e3b34
salinsalins <sanin_andrey@mail.ru>
2022-05-05 13:41:36 +0700

    commit

--- EmulatedLambda.py
+++ EmulatedLambda.py
@@ -15,21 +15,32 @@ class FakeComPort:
         self.lock = Lock()
         self.online = False
         self.last_write = b''
-        self.pv = {self.last_address: 0.0}
-        self.pc = {self.last_address: 0.0}
-        self.mv = {self.last_address: 0.0}
-        self.mc = {self.last_address: 0.0}
-        self.out = {self.last_address: False}
-        self.sn = {self.last_address: str(FakeComPort.SN).encode()}
-        FakeComPort.SN += 1
-        self.id = {self.last_address: FakeComPort.ID}
-        self.t = {self.last_address: time.perf_counter()}
+        self.pv = {}
+        self.pc = {}
+        self.mv = {}
+        self.mc = {}
+        self.out = {}
+        self.sn = {}
+        self.id = {}
+        self.t = {}
         self.write_error = False
+        self.add_device()
 
     def close(self):
         self.last_write = b''
         self.online = False
         return True
+
+    def add_device(self):
+        if self.last_address not in self.pv:
+            self.id[self.last_address] = FakeComPort.ID
+            self.pv[self.last_address] = 0.0
+            self.pc[self.last_address] = 0.0
+            self.mv[self.last_address] = 0.0
+            self.mc[self.last_address] = 0.0
+            self.out[self.last_address] = False
+            self.sn[self.last_address] = str(FakeComPort.SN).encode()
+            FakeComPort.SN += 1
 
     def write(self, cmd, timeout=None):
         self.last_write = cmd
@@ -37,15 +48,7 @@ class FakeComPort:
         try:
             if self.last_write.startswith(b'ADR '):
                 self.last_address = int(self.last_write[4:])
-                if self.last_address not in self.pv:
-                    self.pv[self.last_address] = 0.0
-                    self.pc[self.last_address] = 0.0
-                    self.mv[self.last_address] = 0.0
-                    self.mc[self.last_address] = 0.0
-                    self.out[self.last_address] = False
-                    self.sn[self.last_address] = str(FakeComPort.SN).encode()
-                    FakeComPort.SN += 1
-                    self.id[self.last_address] = self.id[-1]
+                self.add_device()
             elif self.last_write.startswith(b'PV '):
                 self.pv[self.last_address] = float(cmd[3:])
             elif self.last_write.startswith(b'PC '):
@@ -54,8 +57,6 @@ class FakeComPort:
                 self.out[self.last_address] = True
             elif self.last_write.startswith(b'OUT OF') or self.last_write.startswith(b'OUT 0'):
                 self.out[self.last_address] = False
-            elif self.last_write.find(b'?') > 0:
-                pass
             else:
                 self.write_error = True
             self.t[self.last_address] = time.perf_counter()
@@ -75,7 +76,6 @@ class FakeComPort:
             self.last_write = b''
             return b'E1\r'
         if self.last_write.startswith(b'ADR '):
-            # self.last_address = int(self.last_write[3:])
             self.last_write = b''
             return b'OK\r'
         if self.last_write.startswith(b'DVC?'):
